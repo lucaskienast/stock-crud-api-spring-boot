70214e45fed063d2e71649326f3ad6967b8c0c32
LucasKienast <lucas.kienast@hotmail.de>
2022-07-26 23:01:30 +0200

    added tests

--- src/main/java/com/kienast/stockcrudapi/repository/StockRepository.java
+++ src/main/java/com/kienast/stockcrudapi/repository/StockRepository.java
@@ -2,7 +2,9 @@ package com.kienast.stockcrudapi.repository;
 
 import com.kienast.stockcrudapi.model.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     Stock findByTicker(String ticker);
--- src/test/java/com/kienast/stockcrudapi/StockCrudApiApplicationTests.java
+++ src/test/java/com/kienast/stockcrudapi/StockCrudApiApplicationTests.java
@@ -1,13 +1,58 @@
 package com.kienast.stockcrudapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class StockCrudApiApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+
     @Test
-    void contextLoads() {
+    public void getsAllStocks() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/stocks").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+
+    @Test
+    public void getStockByTicker() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/stocks/byTicker/MSFT")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+
+    @Test
+    public void getStockByCompanyName() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/stocks/byName/Microsoft Inc.")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+
+    @Test
+    public void addsNewStock() throws Exception {
+        String newStock = "{\"ticker\":\"AAPL\",\"companyName\":\"Apple Inc.\",\"sharesOutstanding\":5000000,\"pricePerShare\":111.11}";
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/stocks")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(newStock)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
     }
 
 }
